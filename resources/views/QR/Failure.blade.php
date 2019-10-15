82e4e615ec5698847a91c772638efbdea1d382f2
waonngionn <air_mito.0622@icloud.com>
2019-10-15 15:59:13 +0900

    phtogeni bgm hotaru

--- resources/views/QR/Failure.blade.php
+++ resources/views/QR/Failure.blade.php
@@ -5,7 +5,7 @@
 @section('body')
 {{-- body --}}
 <div id="audio_box">
-    <audio src="{{ secure_asset('storage/audio/hotaru.mp3') }}" autoplay loop controls>
+    <audio src="https://photogeni.life/BGM/hotaru.mp3" autoplay loop controls>
         <p>音声の再生に対応したブラウザをインストールしてください。</p>
     </audio>
 </div>
