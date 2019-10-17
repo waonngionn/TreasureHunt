1c9b2d77d6e4417588ec71f1196f9f5503b72e61
waonngionn <air_mito.0622@icloud.com>
2019-10-18 08:48:26 +0900

    mysql設定

--- resources/views/QR/Success.blade.php
+++ resources/views/QR/Success.blade.php
@@ -4,6 +4,26 @@
 
 @section('body')
 {{-- body --}}
+<div class="modal" tabindex="-1" role="dialog">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">モーダルのタイトル</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="閉じる">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div><!-- /.modal-header -->
+      <div class="modal-body">
+        <p>モーダルボディの本文。</p>
+      </div><!-- /.modal-body -->
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">閉じる</button>
+        <button type="button" class="btn btn-primary">変更を保存</button>
+      </div><!-- /.modal-footer -->
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.5.16/p5.min.js"></script>
 <script src="{{secure_asset('/js/success/particle.js')}}"></script>
 <script src="{{secure_asset('/js/success/firework.js')}}"></script>
--- resources/views/layouts/html.blade.php
+++ resources/views/layouts/html.blade.php
@@ -5,9 +5,11 @@
     <meta charset="utf-8">
     <title>@yield('title')</title>
 
-    <link href="{{secure_asset('/css/index.css')}}" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('/css/index.css') }}" rel="stylesheet">
 </head>
 <body>
     @yield('body')
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
